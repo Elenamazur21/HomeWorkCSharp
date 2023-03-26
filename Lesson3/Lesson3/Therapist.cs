c44f54cbe5c737de897a63a159cf918b068082f3
Elena <Lena.Fantom.14.21@mail.ru>
2023-03-26 23:52:38 +0700

    add homework

---
+++ Lesson3/Lesson3/Therapist.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Lesson3
+{
+	public class Therapist : Doctor
+
+	{
+        public Therapist (string name, int age, int exper, string spec) : base(name, age, exper, spec)
+        {
+        }
+
+        public override void Cure(Patient patient)
+        {
+            if (patient.Status == true)
+            {
+                Console.WriteLine("Ты здоров,иди домой");
+
+            }
+            else
+            {
+                patient.Status = true;
+                Console.WriteLine("Ура,чудесная сила исцеления!");
+            }
+        }
+
+
+
+    }
+
+}
+
